4888ab5aa46dff865a44ae4f3365a0e70547fcc7
ju.chao <ju.chao@rytong.com>
2013-06-06 18:10:24 +0800

    test push

--- src/cowboy.erl
+++ src/cowboy.erl
@@ -23,21 +23,21 @@
 %% @doc Start an HTTP listener.
 -spec start_http(any(), non_neg_integer(), any(), any()) -> {ok, pid()}.
 start_http(Ref, NbAcceptors, TransOpts, ProtoOpts)
-		when is_integer(NbAcceptors), NbAcceptors > 0 ->
-	ranch:start_listener(Ref, NbAcceptors,
-		ranch_tcp, TransOpts, cowboy_protocol, ProtoOpts).
+  when is_integer(NbAcceptors), NbAcceptors > 0 ->
+    ranch:start_listener(Ref, NbAcceptors,
+                         ranch_tcp, TransOpts, cowboy_protocol, ProtoOpts).
 
 %% @doc Start an HTTPS listener.
 -spec start_https(any(), non_neg_integer(), any(), any()) -> {ok, pid()}.
 start_https(Ref, NbAcceptors, TransOpts, ProtoOpts)
-		when is_integer(NbAcceptors), NbAcceptors > 0 ->
-	ranch:start_listener(Ref, NbAcceptors,
-		ranch_ssl, TransOpts, cowboy_protocol, ProtoOpts).
+  when is_integer(NbAcceptors), NbAcceptors > 0 ->
+    ranch:start_listener(Ref, NbAcceptors,
+                         ranch_ssl, TransOpts, cowboy_protocol, ProtoOpts).
 
 %% @doc Stop a listener.
 -spec stop_listener(any()) -> ok.
 stop_listener(Ref) ->
-	ranch:stop_listener(Ref).
+    ranch:stop_listener(Ref).
 
 %% @doc Convenience function for setting an environment value.
 %%
@@ -46,8 +46,8 @@ stop_listener(Ref) ->
 %% list used for routing.
 -spec set_env(any(), atom(), any()) -> ok.
 set_env(Ref, Name, Value) ->
-	Opts = ranch:get_protocol_options(Ref),
-	{_, Env} = lists:keyfind(env, 1, Opts),
-	Env2 = [{Name, Value}|lists:keydelete(Name, 1, Env)],
-	Opts2 = lists:keyreplace(env, 1, Opts, {env, Env2}),
-	ok = ranch:set_protocol_options(Ref, Opts2).
+    Opts = ranch:get_protocol_options(Ref),
+    {_, Env} = lists:keyfind(env, 1, Opts),
+    Env2 = [{Name, Value}|lists:keydelete(Name, 1, Env)],
+    Opts2 = lists:keyreplace(env, 1, Opts, {env, Env2}),
+    ok = ranch:set_protocol_options(Ref, Opts2).
